73846edd0ed2120175e494e3b79bc1c6214727fa
Daniel <danmobbs.101@gmail.com>
2021-04-20 08:02:17 +0100

    started WP theme

---
+++ template-parts/Home.php
@@ -0,0 +1,108 @@
+<?php
+/**
+* Template Name: Home
+ */
+
+get_header(); ?>
+
+	<!--Site Wrapper-->
+	<div class="container">
+
+		<section class="py-5">
+			<div class="row">
+				<div class="col-6">
+					<h1 class="display-3">
+					Lorem ipsum dolor sit amet
+					</h1>
+					<p class="lead">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+					Cum sociis natoque penatibus et magnis dis parturient montes. 
+					Nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+					</p>
+				</div>
+				<div class="col-6">
+					<img src="https://via.placeholder.com/550" alt="">
+				</div>
+			</div>
+		</section>
+
+		<section class="py-5">
+			<div class="card-deck">
+				<div class="card mb-3">
+					<img class="card-img-top" src="https://via.placeholder.com/550" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					</div>
+				</div>
+				<div class="card mb-3">
+					<img class="card-img-top" src="https://via.placeholder.com/550" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					</div>
+				</div>
+				<div class="card mb-3">
+					<img class="card-img-top" src="https://via.placeholder.com/550" alt="Card image cap">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					</div>
+				</div>
+			</div>	
+		</section>
+
+		<section class="py-5">
+			<div class="card-group">
+				<div class="card text-white bg-primary mb-3" style="max-width: 25rem;">
+					<div class="card-header">Header</div>
+					<div class="card-body">
+						<h5 class="card-title">Primary card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
+				<div class="card text-white bg-primary mb-3" style="max-width: 25rem;">
+					<div class="card-header">Header</div>
+					<div class="card-body">
+						<h5 class="card-title">Primary card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
+				<div class="card text-white bg-primary mb-3" style="max-width: 25rem;">
+					<div class="card-header">Header</div>
+					<div class="card-body">
+						<h5 class="card-title">Primary card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="py-5">
+			<div class="row">
+				<div class="col-12 text-center">
+					<h2 class="display-3">
+					What our clients say about us 
+					</h2>
+					<p class="lead">
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+					Cum sociis natoque penatibus et magnis dis parturient montes. 
+					Nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
+					</p>
+					<p class="text-muted">
+						<a href="#">Lorem ipsum dolor sit amet, consectetuer adipiscing elit</a>
+					</p>
+					<p class="font-weight-bold" style="32px;">
+						Mary Donald
+					</p>
+				</div>
+			</div>
+		</section>
+
+	</div><!--siteWrapper /-->		
+
+<?php
+get_footer();
